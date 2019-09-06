a0dfbbd1e5207dbd77d9ab72649a3b67e3d25c4a
PHAGUN-JAIN <omgill084@gmail.com>
2019-09-06 16:47:14 +0530

    find greatest of three numbers

---
+++ sign.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+main()
+{int a;
+printf("enter a number");
+scanf("%d",&a);
+if (a>0)
+printf("positive number");
+if(a<0)
+printf("negative number");
+if (a==0)
+{printf("number is zero");}
+}
