9a3f2504081d821c6071fe41a2375e8715add0dc
PHAGUN JAIN <53911145+PHAGUN-JAIN@users.noreply.github.com>
2020-10-17 16:43:37 +0530

    noting new

--- Binary Search/binaryPJ.cpp
+++ Binary Search/binaryPJ.cpp
@@ -3,15 +3,15 @@ using namespace std;
 
 int binarySearch(int arr[], int p, int r, int x)
 {
-	if (r >= l) {
-		int mid = p + (r - l) / 2;
+	if (r >= p) {
+		int mid = p + (r - p) / 2;
 
 
 		if (arr[mid] == x)
 			return mid;
 
 		if (arr[mid] > x)
-			return binarySearch(arr, l, mid - 1, x);
+			return binarySearch(arr, p, mid - 1, x);
 
 		        return binarySearch(arr, mid + 1, r, x);
 	  }
