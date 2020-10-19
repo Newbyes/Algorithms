a2daec966d6f8e8feaf6f3e1a590583ff9d9ac77
Pranit5895 <guptapranit9@gmail.com>
2020-10-19 04:25:22 -0700

    Updated code

--- Insertion-Sort/INsertion-Sort.cpp
+++ Insertion-Sort/INsertion-Sort.cpp
@@ -1,42 +1,33 @@
-// C++ program for insertion sort  
-#include <bits/stdc++.h> 
-using namespace std; 
-  
-/* Function to sort an array using insertion sort*/
-void insertionSort(int arr[], int n)  
-{  
-    int i, key, j;  
-    for (i = 1; i < n; i++) 
-    {  
-        key = arr[i];  
-        j = i - 1;  
-  
-        // conditonal checking 
-        while (j >= 0 && arr[j] > key) 
-        {  
-            arr[j + 1] = arr[j];  
-            j = j - 1;  
-        }  
-        arr[j + 1] = key;  
-    }  
-}  
-  
-// Function to print array
-void printArray(int arr[], int n)  
-{  
-    int i;  
-    for (i = 0; i < n; i++)  
-        cout << arr[i] << " ";  
-    cout << endl; 
-}  
-  
-int main()  
-{  
-    int arr[] = { 12, 11, 13, 5, 6 };  
-    int n = sizeof(arr) / sizeof(arr[0]);  
-  
-    insertionSort(arr, n);  
-    printArray(arr, n);  
-  
-    return 0;  
-}  
+#include<iostream.h>
+#include<conio.h>
+void insertion(int A[],int size);
+void main()
+{
+cout<<"Enter the size of the array\n";
+int size;
+cin>>size;
+int arr[80];
+for(int i=0;i<size;i++)
+{
+cout<<"Enter the element at position "<<i+1<<" of the array\n";
+cin>>arr[i];
+}
+void insertion(int A[],int size)
+{
+int i,j,key;
+for(i=1;i<size;i++)
+{
+key=A[i];
+j=i-1;
+while(j>=0 && A[j]>key)
+{
+A[j+1]=A[j];
+j=j-1;
+}
+A[j+1]=key;
+}
+cout<<"\nArray after sorting is: ";
+for(int m=0;m<size;m++)
+cout<<A[m]<<" ";
+}
+
