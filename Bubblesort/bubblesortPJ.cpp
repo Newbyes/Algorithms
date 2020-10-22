aea4a12a3415ef5b47c644f8836c6302a38f1c7c
PHAGUN-JAIN <omgill084@gmail.com>
2020-10-22 18:41:15 +0530

    completed bubble sort and specified return type of swap and sorting function to void

--- Bubblesort/bubblesortPJ.cpp
+++ Bubblesort/bubblesortPJ.cpp
@@ -2,11 +2,19 @@
  #include<iostream>
  using namespace std;
 
+void swap(int *x, int *y){
+	int temp= *x;
+	*x=*y;
+	*y=temp;
+}
 
-
-
-int bubbleSort(int arr[], int n){
-
+void bubbleSort(int arr[], int n){
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n-i-1; j++){
+			if(arr[j]>arr[j+1])
+				swap(&arr[j],&arr{j+1]);
+		}
+	}
 }
 
  int main(){
