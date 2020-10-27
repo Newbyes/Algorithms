39d065c9eae5f7e4ef45fc2739ac2d52c7cf9603
AkshatGoyal <58101087+AkshatGoyal1018@users.noreply.github.com>
2020-10-28 01:06:47 +0530

    a little error left

--- quicksort/quicksortA.cpp
+++ quicksort/quicksortA.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-int arr[10]={35,64,24,86,30,82,11,96,4,57};
-
 int swap(int *x,int *y)
 {
 	*x = *x + *y;
@@ -13,12 +11,49 @@ int swap(int *x,int *y)
 	return 0;
 }
 
-int parition(int h, int l)
+int partition(int a[],int h, int l)
 {
-	int pivot = arr[l];
+	int pivot = a[l];
 	int i = l+1;
-	for(int j = h; j>=i;j++)
+	for(int j = l+2;j<=h;j++)
 	{
-		
+		if(a[j]<pivot)
+		{
+			swap(&a[i], &a[j]);
+			i++;	
+		}	
 	} 
+	swap(&a[i-1], &a[l]);
+	return(i-1);
+}
+
+int quicksort(int ar[],int low,int high)
+{
+	if(low<high)
+	{
+		int pi = partition(ar,low,high);
+		
+		quicksort(ar,low,pi-1);
+		quicksort(ar,pi+1,high);
+	}
+	return  0;
+}
+
+int printarr(int array[],int size)
+{
+	for(int x=0;x<size;x++)
+		cout<<array[x]<<" ";
+	cout<<endl;
+	return 0;
+}
+int main()
+{
+	int arr[10]={35,64,24,86,30,82,11,96,4,57};
+	int n=10;
+	cout<<"Unsorted array: ";
+	printarr(arr,n);
+	quicksort(arr,0,n-1);
+	cout<<"Sorted array: ";
+	printarr(arr,n);	
+	return 0;
 }
