dac497c5afeb1ba31f635d16b0ec232bc570413b
PHAGUN-JAIN <omgill084@gmail.com>
2020-10-30 21:02:52 +0530

    added main function and declared other  necessary functions

---
+++ quicksort/quiksortPJ.cpp
@@ -0,0 +1,20 @@
+
+ #include<iostream>
+ using namespace std;
+
+int partition(int arr[], low, high){
+
+}
+
+int quick-sort(int arr[], low, high){
+
+}
+int main(){
+    int arr[]={45,48,56,92,18,19,3,1};
+    int n=size0f(arr)/sizeof(arr[0]);
+    quick-sort(arr, 0, n);
+    cout<<"Sorted Array:"<<endl;
+    for(int i=0; i<n; i++){
+        cout<<arr[i];
+    }
+}
