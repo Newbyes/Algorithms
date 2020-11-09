6fdd6f6b0348e62591d94b67dc503eeed27d29dd
PHAGUN-JAIN <omgill084@gmail.com>
2020-11-09 21:15:36 +0530

    added frame for insertion

---
+++ Insertion-Sort/insertionPJ.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void print(int arr[], int n){
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+int main(){
+	int arr[]={46,98,22,48,4,6,47};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	cout<<"unsorted Array:"<<endl;
+	print(arr, size);
+
+	insertionsort(arr, size);	//insertion sort is a inplace sorting algorithm
+
+	cout<<"sorted array:"<<endl;
+	print(arr, size);
+
+return 0;
+
+}
