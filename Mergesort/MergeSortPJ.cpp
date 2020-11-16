964a0c79d629fbdf8416567614055b53a71c1216
PHAGUN-JAIN <omgill084@gmail.com>
2020-11-16 16:28:44 +0530

    fixed some syntax err and logical err

--- Mergesort/MergeSortPJ.cpp
+++ Mergesort/MergeSortPJ.cpp
@@ -2,7 +2,31 @@
 using namespace std;
 
 void merge(int arr[], int front, int rear, int mid){
-  int L[],R[];
+  int size1=mid-front+1;  //earlier tried to assign values directly in left and right array but got stuck in assigning values to right array without declaring sizes first
+  int size2=rear-mid;     //also direct assigning worked only for first iteration
+  int L[size1],R[size2];  //declaring left and right arrays
+//assigning values to left array
+  for(int i=0; i<=size1; i++){
+    L[i]=arr[front+i];      //front+1 *front for indicatingthe starting index of original array *+i for incrementing the head fornext iteration
+  }
+//assigning values to right array
+  for(int j=0; j<size2; j++){
+    R[j]=arr[mid+1+j];
+  }
+
+  int i=0,j=0,k=front;
+  while(i<size1 && j<size2){
+      if(L[i] < R[j]){
+        arr[k]=L[i];
+        i++;
+        k++;
+      }
+      else{
+      arr[k]=R[j];
+      j++;
+      k++;
+      }
+  }
 }
 
 void mergesort(int arr[],int front, int rear){
@@ -23,9 +47,9 @@ int main(){
   int arr[]={20,176,96,4,2,0,46};
   int size=sizeof(arr)/sizeof(arr[0]);
   cout<<"array before sorting:"<<endl;
-  print(arr,n);
-  mergesort(arr);
+  print(arr,size);
+  mergesort(arr,0, size-1);
   cout<<"array after sorting:"<<endl;
-  print(arr, n);
+  print(arr, size);
   return 0;
 }
