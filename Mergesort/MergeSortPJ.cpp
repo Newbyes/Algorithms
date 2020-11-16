031be0817bbe65d8d5874f867a179448633b1730
PHAGUN-JAIN <omgill084@gmail.com>
2020-11-16 15:57:12 +0530

    pulled remote and updated mergesortPJ

--- Mergesort/MergeSortPJ.cpp
+++ Mergesort/MergeSortPJ.cpp
@@ -1,11 +1,24 @@
 #include<iostream>
 using namespace std;
 
+void merge(int arr[], int front, int rear, int mid){
+  int L[],R[];
+}
+
+void mergesort(int arr[],int front, int rear){
+    int mid=(front+rear)/2;
+      mergesort(arr, front, mid);
+      mergesort(arr, mid+1, rear);
+
+      merge(arr, front, rear, mid);
+}
+
 void print(int arr[], int n){
   for(int i=0; i<n; i++){
     cout<<arr[i]<<" ";
   }
 }
+
 int main(){
   int arr[]={20,176,96,4,2,0,46};
   int size=sizeof(arr)/sizeof(arr[0]);
